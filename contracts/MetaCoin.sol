71abd24845d3196b637808b15f5bf7c8a84daaea
Carlos Hernandez <learnercys@gmail.com>
2018-05-26 19:06:55 -0600

    Finish the Getting Started Tutorial present here http://truffleframework.com/docs/getting_started/project.

--- contracts/MetaCoin.sol
+++ contracts/MetaCoin.sol
@@ -8,27 +8,35 @@ import "./ConvertLib.sol";
 // token, see: https://github.com/ConsenSys/Tokens. Cheers!
 
 contract MetaCoin {
-	mapping (address => uint) balances;
+    mapping (address => uint) balances;
 
-	event Transfer(address indexed _from, address indexed _to, uint256 _value);
+    event Transfer(address indexed _from, address indexed _to, uint256 _value);
 
-	constructor() public {
-		balances[tx.origin] = 10000;
-	}
+    constructor() public {
+        // TODO security/no-tx-orign: Consider using 'msg.sender' in placeof `tx.origin`.
+        // not sure how, still newbie. 
+        // It should be `balances[msg.sender] = 10000` or something like it.
+        // Also, looks like this operation is defining the initial MetaCoin balance set 
+        // to 10000 coins.
+        balances[tx.origin] = 10000;
+    }
 
-	function sendCoin(address receiver, uint amount) public returns(bool sufficient) {
-		if (balances[msg.sender] < amount) return false;
-		balances[msg.sender] -= amount;
-		balances[receiver] += amount;
-		emit Transfer(msg.sender, receiver, amount);
-		return true;
-	}
+    function sendCoin(address receiver, uint amount) public returns(bool sufficient) {
+        if (balances[msg.sender] < amount) return false;
+        balances[msg.sender] -= amount;
+        balances[receiver] += amount;
+        emit Transfer(msg.sender, receiver, amount);
+        return true;
+    }
 
-	function getBalanceInEth(address addr) public view returns(uint){
-		return ConvertLib.convert(getBalance(addr),2);
-	}
+    function getBalanceInEth(address addr) public view returns(uint){
+        // it seems that the rate is twice the native coin
+        // in this case the native coin is `MetaCoin`. Not sure,
+        // need to investigate more.
+        return ConvertLib.convert(getBalance(addr),2);
+    }
 
-	function getBalance(address addr) public view returns(uint) {
-		return balances[addr];
-	}
+    function getBalance(address addr) public view returns(uint) {
+        return balances[addr];
+    }
 }
